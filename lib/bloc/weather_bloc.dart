fe9298bcc1334fe13a4ea4cad57ed50160014ae2
diegitsen <diegitsen@gmail.com>
2020-02-07 19:45:30 -0500

    furst

---
+++ lib/bloc/weather_bloc.dart
@@ -0,0 +1,22 @@
+import 'package:rxdart/rxdart.dart';
+import '../model/weather_response_model.dart';
+import 'package:weather_app_bloc/persistance/repository.dart';
+
+class WeatherBloc {
+  Repository _repository = Repository();
+
+  final _weatherFetcher = PublishSubject<WeatherResponse>();
+
+  Observable<WeatherResponse> get weather => _weatherFetcher.stream;
+
+  fetchLondonWeather() async {
+    WeatherResponse weatherResponse = await _repository.fetchLondonWeather();
+    _weatherFetcher.sink.add(weatherResponse);
+  }
+
+  dispose() {
+    _weatherFetcher.close();
+  }
+}
+
+final weatherBloc = WeatherBloc();
